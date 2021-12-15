f578c3afb91215322c96d1849de7d9122c362729
badboy7056 <96168731+badboy7056@users.noreply.github.com>
2021-12-15 13:30:11 +0530

    Add project files.

---
+++ cafe/Controllers/cafe.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cafe.Controllers
+{
+    public class cafe : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
